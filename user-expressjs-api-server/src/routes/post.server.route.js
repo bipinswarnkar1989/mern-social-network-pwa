1136daa84018749ec98f04655a5fc8d45e79bdff
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-11-11 12:36:01 +0530

    Added Post Server Route

--- user-expressjs-api-server/src/routes/post.server.route.js
+++ user-expressjs-api-server/src/routes/post.server.route.js
@@ -1,6 +1,8 @@
-//import userController from '../controllers/user.server.controller';
-///const userCtrl = new userController();
-var fs = require('fs');
+import mediaController from '../controllers/media.server.controller';
+import postController from '../controllers/post.server.controller';
+const mediaCtrl = new mediaController();
+const postCtrl = new postController();
+
 class postRoutes {
    constructor(router){
     this.router = router;
@@ -9,28 +11,7 @@ class postRoutes {
    
    registerRoutes(){
     this.router.route('/posts/v1/')
-    .post(function (req, res, next) {
-        var fstream;var data = '';
-  if (req.busboy) {
-
-    req.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      fstream = fs.createWriteStream(__dirname + '/../../public/' + filename);
-      file.pipe(fstream);
-
-      fstream.on('data', function (chunk) {
-        data += chunk;
-      });
-      fstream.on('close', function(){
-        console.log('file ' + filename + ' uploaded');
-      });
-    });
-    req.busboy.on('finish', function(){
-      console.log('finish, files uploaded ');
-      res.json({ success : true});
-    });
-    req.pipe(req.busboy);
-  }
-   });
+    .post(postCtrl.createPost,mediaCtrl.createMedia,postCtrl.viewResults);
 }
 
 }
